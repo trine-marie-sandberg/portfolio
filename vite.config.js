2f3d82a042b1c03d83216bfc1061b450bea49a13
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-12-04 15:09:52 +0100

    upd. path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://trine-marie-sandberg.github.io/portfolio",
+  base: "https://trine-marie-sandberg.github.io/portfolio/",
   plugins: [react()]
 })
 //deploy using gh-pages library: https://medium.com/@arifiansaputra43/how-to-host-vite-react-in-github-pages-with-gh-pages-a8a0566c493f
