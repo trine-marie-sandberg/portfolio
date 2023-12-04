5211be41bb1ba013c3fa5dc70dea67fcdf1d9634
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-12-04 13:40:48 +0100

    upd. package.json and vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "http://trine-marie-sandberg.github.io/portfolio",
+  plugins: [react()]
 })
