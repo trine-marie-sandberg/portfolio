7c373415824260088c73e6b61395fff3f598577d
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2025-01-24 20:03:21 +0100

    update profile img

--- src/pages/about/style.js
+++ src/pages/about/style.js
@@ -1,22 +1,25 @@
 import styled from "styled-components";
 
 export const Portrait = styled.div`
-  background-size: contain;
-  background-image: url("me.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("me2.jpg");
   border-radius: 100%;
-  border: 1px solid ${(props) => props.theme.color.dark};
+  /* border: 2px solid ${(props) => props.theme.color.secondary}; */
+  border: 5px solid rgb(255, 255, 255);
 `;
 export const ImgBg = styled.div`
-  background-color: rgba(0, 0, 0, 0.3);
+  /* background-color: rgba(0, 0, 0, 0.1); */
   /* border: 1px solid ${(props) => props.theme.color.dark}; */
-  /* border: 2px solid ${(props) => props.theme.color.secondary}; */
-  height: 150px;
-  width: 150px;
-  border-radius: 100%;
+  /* border: 4px solid ${(props) => props.theme.color.secondary}; */
+  border: 4px solid rgb(99, 17, 88);
+  border-radius: 10px;
+  height: 120px;
+  width: 120px;
   /* border-radius: 45% 45% 45% 5%; */
   @media(max-width: 500px) {
-    height: 100px;
-    width: 100px;
+    height: 75px;
+    width: 75px;
   }
 `;
 export const FlexImgHeading = styled.div`
