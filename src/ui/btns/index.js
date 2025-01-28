e132ae5b51368337ed9831aba1f537ad3ea5b3f1
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2025-01-28 13:52:48 +0100

    upd. style btn.

--- src/ui/btns/index.js
+++ src/ui/btns/index.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 //Primary
 export const BtnPrimary = styled.div`
   width: fit-content;
-  background-color: ${(props) => props.theme.color.light};
+  /* background-color: ${(props) => props.theme.color.light}; */
+  background-color: rgb(43, 1, 44);
+  /* border: 1px solid ${(props) => props.theme.color.primary}; */
   border: 1px solid ${(props) => props.theme.color.dark};
   color: ${(props) => props.theme.color.black};
   border-radius: 5px;
@@ -11,15 +13,17 @@ export const BtnPrimary = styled.div`
   margin: 5px;
   cursor: pointer;
   & :hover {
-    box-shadow: rgba(63, 152, 186, 0.5) 0px 7px 20px 0px;
+    /* box-shadow: rgba(63, 152, 186, 0.5) 0px 7px 20px 0px; */
+    box-shadow: rgba(50, 118, 145, 0.83) 0px 7px 20px 0px;
     border-radius: 5px;
   }
   & button {
     margin: 0;
     padding: 0;
-    background-color: ${(props) => props.theme.color.light};
+    /* background-color: ${(props) => props.theme.color.light}; */
+    background-color: rgb(43, 1, 44);
     border: none;
-    color: ${(props) => props.theme.color.black};
+    color: ${(props) => props.theme.color.font};
     font-size: 18px;
     padding: 5px 15px;
     width: 100%;
